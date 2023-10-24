67ecc1a82cbb4aa58d558d843622b0eb5a6ec157
Charly3012 <carlocoronadosilva975@gmail.com>
2023-10-23 23:22:06 -0600

    Organizacion de paquetes

---
+++ src/main/java/Controlador/Hola.java
@@ -0,0 +1,16 @@
+package Controlador;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Hola extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
