1bc2a608324bb879009f6d2e93a7d5483a9a190a
desarrollador3he <desarrollador3he>
2023-11-06 16:06:58 -0600

    Actualizacion

---
+++ src/main/java/controlador/ControladorRegistroVentas.java
@@ -0,0 +1,38 @@
+package controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TableColumn;
+
+public class ControladorRegistroVentas {
+
+    @FXML
+    private Button btnBusca;
+
+    @FXML
+    private MenuButton btnMenu;
+
+    @FXML
+    private Button btnpagar;
+
+    @FXML
+    private TableColumn<?, ?> tbltablaregistroventas;
+
+    @FXML
+    void clickBuscar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickMenu(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickpagar(ActionEvent event) {
+
+    }
+
+}
