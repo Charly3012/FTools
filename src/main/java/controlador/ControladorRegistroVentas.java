5d6236bba360c10c4eaca3de329f3cc51ad6a282
desarrollador3he <desarrollador3he>
2023-11-25 12:51:27 -0600

    La cague jaja

--- src/main/java/controlador/ControladorRegistroVentas.java
+++ src/main/java/controlador/ControladorRegistroVentas.java
@@ -1,8 +1,11 @@
 package controlador;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,17 +13,24 @@ import javafx.scene.control.MenuButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import modelo.Producto;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 /**
  * clase encargada de ser el controlador del Registro de ventas
  * @author Jhonatan Solis
  * @version 1.0
  */
-public class ControladorRegistroVentas {
+public class ControladorRegistroVentas implements Initializable {
 
     @FXML
     private AnchorPane anchorDetalleVenta;
@@ -59,13 +69,16 @@ public class ControladorRegistroVentas {
     private TableView<?> tblDetalleVenta;
 
     @FXML
-    private TableView<?> tblProductosDisponibles;
+    private TableView<Producto> tblProductosDisponibles;
 
     @FXML
     private TextField txtBuscarProducto;
 
     @FXML
     private TextField txtTotal;
+
+    @FXML //Lista que se muestra de productos
+    private ObservableList<Producto> productosVista;
 
     /**
      * Maneja el evento de clic en el botón de búsqueda.
@@ -84,6 +97,24 @@ public class ControladorRegistroVentas {
      */
     @FXML
     void clickPagar(ActionEvent event) {
+
+    }
+    public void initialize(URL url, ResourceBundle rb) {
+
+        assert colPrecioUnitarioDisp != null : "fx:id=\"colPrecioUnitarioDisp \" was not injected: check your FXML file 'VistaRegistroComprasVentas.fxml'.";
+        assert colInventarioDisp != null : "fx:id=\"colInventarioDisp\" was not injected: check your FXML file 'VistaRegistroComprasVentas.fxml'.";
+        assert colCategoriaDisp != null : "fx:id=\"colCategoriaDisp\" was not injected: check your FXML file 'VistaRegistroComprasVentas.fxml'.";
+        assert colProuctoDisp!= null : "fx:id=\"colProuctoDisp\" was not injected: check your FXML file 'VistaRegistroComprasVentas.fxml'.";
+        assert tblProductosDisponibles != null : "fx:id=\"tblProductosDisponibles\" was not injected: check your FXML file 'VistaRegistroComprasVentas.fxml'.";
+        assert txtBuscarProducto != null : "fx:id=\"txtBuscarProducto\" was not injected: check your FXML file 'VistaRegistroComprasVentas.fxml'.";
+
+        iniciarDatosObservables();
+        persistenciaLeer();
+
+
+
+
+
 
     }
 
@@ -110,4 +141,44 @@ public class ControladorRegistroVentas {
             throw new RuntimeException(e);
         }
     }
+    public void persistenciaLeer(){
+        //Persistencia - Leer el archivo de datos
+        try{
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("src/main/resources/persistencia/gestionInventarios.cja"));
+            ArrayList<Producto> productosGuardar = (ArrayList<Producto>) ois.readObject();
+            if (productosVista == null) {
+                productosVista = FXCollections.observableArrayList();
+            }
+            productosVista.addAll(productosGuardar);
+
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+    }
+
+    public void iniciarDatosObservables(){
+        //Iniciar la visualización de objetos en la tabla
+        productosVista = FXCollections.observableArrayList(); //Se tiene que crear otro arraylist que va a ser el que se va a visualizar en la vista
+
+        this.tblProductosDisponibles.setItems(productosVista); //Para setear los elementos de nuestro array original al que se muestra en pantalla
+
+
+        //Mapeo de las columnas de la tabla con los atributos de los objetos persona
+        this.colProuctoDisp.setCellValueFactory((new PropertyValueFactory<>("Producto")));
+        this.colCategoriaDisp.setCellValueFactory((new PropertyValueFactory<>("Categoria")));
+        this.colInventarioDisp.setCellValueFactory((new PropertyValueFactory<>("Inventario")));
+        this.colPrecioUnitarioDisp.setCellValueFactory((new PropertyValueFactory<>("Precio unitario")));
+
+
+
+    }
+
+
+
+
+
+
 }
