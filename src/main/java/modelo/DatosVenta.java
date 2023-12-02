27e8ffcb954f3d3256c1428f3af630467cbd9ace
desarrollador3he <desarrollador3he>
2023-12-02 11:39:00 -0600

    Ayudaa

---
+++ src/main/java/modelo/DatosVenta.java
@@ -0,0 +1,18 @@
+package modelo;
+
+import java.io.Serializable;
+
+public class DatosVenta extends Producto implements Serializable {
+
+
+    /*public DatosVenta(String nombre, int cantExistencia, double precioUnitario) {
+
+        super(nombre, cantExistencia, precioUnitario);
+
+    }*/
+
+
+
+
+
+}
